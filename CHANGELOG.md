9370382003921819f4f4f4286796f6acd92aea6a
Luis M <lgmm.93@gmail.com>
2026-03-16 21:47:58 -0600

    published missing entrypoints

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,13 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ---
 
+## [1.0.1] - 2026-03-16
+
+### Added
+- Exported `Public`, `CurrentUser`, `JwtAuthGuard`, `RefreshGuard`, `UserEntity`, `SessionEntity` from the root package entry point
+
+---
+
 ## [1.0.0] - 2026-03-16
 
 First stable release.
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 export { AuthModule, type AuthModuleAsyncOptions } from "./module/auth.module";
 export { type AuthModuleOptions, type JwtOptions, type CurrentUserMode } from "./module/auth.module.options";
+export { Public, CurrentUser } from "./decorators/index";
+export { JwtAuthGuard, RefreshGuard } from "./guards/index";
+export { UserEntity, SessionEntity } from "./entities/index";
