0353815f2f38d1d708defa6cafe659c9e1af9e7f
Luis M <lgmm.93@gmail.com>
2026-03-28 19:00:23 -0600

    export the session service

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export { AuthModule, type AuthModuleAsyncOptions } from "./module/auth.module";
 export { type AuthModuleOptions, type JwtOptions, type CurrentUserMode } from "./module/auth.module.options";
 export { Public, CurrentUser } from "./decorators/index";
 export { AuthService } from "./services/auth.service";
+export { SessionService } from "./services/session.service";
 export { JwtAuthGuard, RefreshGuard } from "./guards/index";
 export { UserEntity, SessionEntity } from "./entities/index";
